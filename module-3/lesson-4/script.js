3b165591bb8995a22ef0ecd1881a60a89b18f352
mukhriddin-dev <khodievm@gmail.com>
2023-06-23 09:43:32 +0500

    part 1

---
+++ module-3/lesson-4/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+
+// localStorage   .setItem( key, value) .getItem( key ) , 
+
+// localStorage.setItem("myname",'Mukhriddin');
+// localStorage.setItem("age", 24);
+// localStorage.setItem('job', "programmer");
+
+
+// let name=localStorage.getItem("myname");
+// let age=localStorage.getItem("age");
+// let job=localStorage.getItem("job");
+
+
+// localStorage.removeItem("myname");
+// localStorage.removeItem("age");
+// localStorage.removeItem("job");
+
+
+// localStorage.clear()
+
+// console.log(localStorage.key())
+
+// console.log(window)
+
+
+
+
+// console.log(name,age, job)
+
+
+// sessionStorage.setItem("myname",'Mukhriddin');
+// sessionStorage.setItem("age", 24);
+// sessionStorage.setItem('job', "programmer");
+
+// document.cookie = "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
+// console.log(window.document.cookie)
