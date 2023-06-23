4f54d446917e6b4856b08987009c18dd4aed4a25
mukhriddin-dev <khodievm@gmail.com>
2023-06-23 14:51:20 +0500

    ok

--- module-3/lesson-4/script.js
+++ module-3/lesson-4/script.js
@@ -1,4 +1,4 @@
-"use strict";
+
 
 
 // localStorage   .setItem( key, value) .getItem( key ) , 
@@ -23,10 +23,6 @@
 // console.log(localStorage.key())
 
 // console.log(window)
-
-
-
-
 // console.log(name,age, job)
 
 
@@ -36,3 +32,51 @@
 
 // document.cookie = "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
 // console.log(window.document.cookie)
+
+// "use strict";
+
+
+let head=document.querySelector('#head'),
+    text=document.querySelector('#text');
+    themSelect=document.querySelector('#them');
+
+    
+    function selectThem(){  
+        if(localStorage.getItem('them')==='dark'){
+            head.style.cssText="background:#000000 !important; color:#fff;"
+            text.style.cssText="background:#000000 !important; color:#fff;"
+            
+        }else{
+            head.style.cssText="background:blue !important; color:#fff;"
+            text.style.cssText="background:yellow !important; color:#000000;"
+        }
+    }
+  
+    selectThem()
+
+
+    themSelect.addEventListener('change', (e)=>{
+
+        localStorage.setItem('them', e.target.value)
+
+        selectThem()
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
