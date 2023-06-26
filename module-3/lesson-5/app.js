ee0d7f9274ecb09e5a734456d1fe7d2e2474b058
mukhriddin-dev <khodievm@gmail.com>
2023-06-26 09:22:50 +0500

    ok

---
+++ module-3/lesson-5/app.js
@@ -0,0 +1,104 @@
+"use strict";
+
+// TDZ
+
+// function numbers(){
+//     return 1;
+
+// }
+
+// // Generator functions ->
+
+// function * letters(){
+//    yield "A";
+//    yield "B";
+//    yield "C";
+//    yield "D";
+// }
+
+
+// const letter=letters();
+
+// // console.log(letter.throw(new Error("Nimadur xato")));
+// console.log(letter.next());
+// console.log(letter.next());
+// console.log(letter.next());
+// console.log(letter.next());
+
+
+// // no curried functions
+
+
+// function funcE(a,b,c,d){
+//     return a+b+c+d;
+// }
+
+// console.log(funcE(1,2,3,4))
+
+
+// /// Curriyng  functions
+
+
+// function funcA(a){
+//     return function(b){
+//         return function(c){
+//             return function(d){
+//                 return a+b+c+d;
+//             }
+//         }
+//     }
+// }
+
+// console.log(funcA(1)(2)(3)(4))
+
+
+// const curriedFunc=a=>b=>c=>d=>a+b+c+d;
+
+// console.log(curriedFunc(1)(2)(3)(4))
+
+
+// /// Pipe function
+
+// const funcC=a=>a+10;
+// const funcD=a=>a-2;
+// const funcS=a=>a*4;
+
+// console.log(funcC(funcD(funcS(10)))) // 48
+
+
+// // pure function
+
+// function pureFun(a){
+// return a*10;
+// }
+
+// console.log(pureFun(10))
+
+// let sum=10;
+
+// // impure function
+
+// function impureFun(a){
+//     return sum+a;
+// }
+
+
+// console.log( impureFun(10))
+
+
+// Closure 
+
+// let num = 11111;
+
+// function ABS() {
+
+  
+  
+//     function a(){
+//         console.log(num)
+//     }
+    
+//     a()x
+// }
+
+// ABS()
